4c013aacabacd1de01ed572971b4321c3af049d4
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-17 16:43:48 -0300

    media de 3 numeros

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace media_sobre_3_notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double notaA, notaB, notaC, media;
+
+            Console.WriteLine("DIGITE A PRIMEIRA NOTA");
+            notaA = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("DIGITE A segunda NOTA");
+            notaB = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A terceira NOTA");
+            notaC = double.Parse(Console.ReadLine());
+
+
+
+
+            media = ((notaA * 2.0 + notaB * 3.0 + notaC * 5.0) / 10.0);
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
+            // ToString("F1",CultureInfo.InvariantCulture));  pode ser utilizado somente na //impressão do resultado ao inves de usar em todos as variaveis
+
+        }
+    }
+}
